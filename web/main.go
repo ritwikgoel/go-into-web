2aa3a48d4f8192244ab9c72a20ff47eda4b431a8
tangobeer <ritwikgoel@yahoo.com>
2020-11-20 14:05:43 +0530

    feat: saving loading and html tempaltes

--- web/main.go
+++ web/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	
 	"io/ioutil"
 	"log"
 	"net/http"
+	"html/template"
 )
 //Page struct 
 type Page struct {
@@ -15,6 +16,10 @@ type Page struct {
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
 }
 
 func loadPage(title string) (*Page, error) {
@@ -27,12 +32,34 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+    title := r.URL.Path[len("/view/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w,"edit",p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/save/"):]
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    p.save()
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/",editHandler)
+	//http.HandleFunc("/save/",saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
